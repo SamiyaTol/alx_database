79f5338adb834c507e074c79ae474ef571f08b82
SamiyaTol <148984827+SamiyaTol@users.noreply.github.com>
2024-02-04 22:01:59 +0100

    Create 3-force_name.sql

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,8 @@
+
+-- Genrating table force_name
+-- data in table: id INT, name VARCHAR(256) can’t be null
+
+CREATE TABLE IF NOT EXISTS force_name (
+       id INT,
+       name VARCHAR(256) NOT NULL
+);
